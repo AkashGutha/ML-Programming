ef410821c32e9a67ec45db67413c5b9a750ccfa2
AkashGutha <akash.gutha@outlook.coma>
2017-09-26 04:38:08 +0530

    move to es6 modules

--- test/LuisQuery.test.js
+++ test/LuisQuery.test.js
@@ -1,10 +1,5 @@
-let LuisQuery = require("./../build/LuisQuery")
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
-let expect = chai.expect;
-
-chai.use(chaiHttp);
+import { expect } from "chai"
+import * as  LuisQuery from "./../build/LuisQuery"
 
 describe('payload passed to the getQuery function', function () {
     let url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4c7b2108-85f7-4a47-b509-f7ef053de4d8?subscription-key=d73b4400d6d745c6b06758e474492522&verbose=true&timezoneOffset=0&spellCheck=true&q=";
@@ -18,7 +13,7 @@ describe('payload passed to the getQuery function', function () {
     
     it('should throw an error when no argument is passed', function (done) {
         expect(
-            new LuisQuery().getQuery()
+            LuisQuery.getQuery()
         ).to.throw("Payload error: Either empty or undefined")
     });
     
